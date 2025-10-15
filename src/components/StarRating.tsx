77ac6a9b75c7885413b6864eea80fe4c664815d8
luizacavalcantee <cavalcanteluiza13@gmail.com>
2025-10-15 15:31:57 -0300

    Add Star component and integrate it into StarRating component

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
+import Star from "./Star";
+
 const containerStyle = {
   display: "flex",
-  alignItems: "center",
-  gap: "1rem",
 };
 
 const starStyle = {
@@ -9,26 +10,56 @@ const starStyle = {
   gap: "4px",
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
-};
-
 interface StarRatingProps {
   maxRating?: number;
+  color?: string;
+  size?: number;
+  className?: string;
+  messages?: string[];
+  defaultRating?: number;
+  onSetRating?: (rating: number) => void;
 }
 
-export default function StarRating({ maxRating = 5 }: StarRatingProps) {
+export default function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 20,
+  className,
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}: StarRatingProps) {
+  const [rating, setRating] = useState(1);
+  const [tempRating, setTempRating] = useState(0);
+
+  function handleRating(rating: number) {
+    setRating(rating);
+    onSetRating?.(rating);
+  }
+
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size}px`,
+  };
+
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starStyle}>
         {Array.from({ length: maxRating }, (_, index) => (
-          <span key={index} className={index < 4 ? "filled" : ""}>
-            ★
-          </span>
+          <Star
+            key={index}
+            full={tempRating ? tempRating >= index + 1 : rating >= index + 1}
+            onRate={() => handleRating(index + 1)}
+            onHoverIn={() => setTempRating(index + 1)}
+            onHoverOut={() => setTempRating(0)}
+            color={color}
+            size={size}
+          />
         ))}
       </div>
-      <p style={textStyle}>10</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
